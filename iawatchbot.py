a5ca504556520d469805f5944e5c3e83f8c375b6
Daniel <daniel.m@archive.org>
2011-06-02 18:16:54 +0000

    watching works now

--- iawatchbot.py
+++ iawatchbot.py
@@ -8,6 +8,7 @@ import urllib
 import json
 import os.path
 import smtplib
+import string
 
 def send_email(fromaddr, toaddrs, subj, message):
     msg = ("Subject: %s\r\nFrom: %s\r\nTo: %s\r\n\r\n" % (subj, fromaddr, ", ".join(toaddrs)))
@@ -37,6 +38,17 @@ s = time()
 g = open("logs/%s" % t, 'w')
 g.write('Started at time %r\n' % t)
 
+# Get the list of admins
+k = urllib.urlopen("http://openlibrary.org/usergroup/admin.json")
+l = json.JSONDecoder().decode(k.read())
+whitelist = []
+for member in l['members']:
+    whitelist.append(member['key'])
+m = urllib.urlopen("http://openlibrary.org/usergroup/api.json")
+n = json.JSONDecoder().decode(m.read())
+for member in n['members']:
+    whitelist.append(member['key'])
+
 # Query for recent changes
 f = urllib.urlopen("http://openlibrary.org/recentchanges.json?limit=1000")
 x = json.JSONDecoder().decode(f.read())
@@ -46,6 +58,14 @@ for y in x:
     if y['id'] <= last_id:
         break
     g.write("Checking id %r\n" % y['id'])
+    author = "[None]"
+    if y['author'] is not None:
+        author = "http://openlibrary.org%s" % y['author']['key']
+    elif y['ip'] is not None:
+        author = y['ip']
+    if y['author'] is not None and y['author']['key'] in whitelist:
+        g.write("Status: fine\n")
+        continue
     for z in y['changes']:
         rev = z['revision']
 
@@ -63,16 +83,40 @@ for y in x:
                 if c.has_key("ocaid"):
                     if c["ocaid"] != d["ocaid"]:
                         g.write("Status: ocaid modified\n")
-                        send_email("daniel.m@archive.org", ["openlibrary@archive.org"], "IA id modified", "The IA id has been modified for the following edition: http://openlibrary.org%s" % z['key'])
+                        send_email("daniel.m@archive.org", ["openlibrary@archive.org"], "IA id modified", "The IA id has been modified by %s for the edition http://openlibrary.org%s with the following comment: '%s'" % (author, z['key'], y['comment']))
                     else:
                         g.write("Status: fine\n")
                 else:
                     g.write("Status: ocaid deleted\n")
-                    send_email("daniel.m@archive.org", ["openlibrary@archive.org"], "IA id deleted", "The IA id has been deleted for the following edition: http://openlibrary.org%s" % z['key'])
+                    send_email("daniel.m@archive.org", ["openlibrary@archive.org"], "IA id deleted", "The IA id has been deleted by %s for the edition http://openlibrary.org%s with the following comment: '%s'" % (author, z['key'], y['comment']))
+            else:
+                g.write("Status: fine\n")
+        elif z['key'][:9] == "/works/OL":
+            url = "http://openlibrary.org" + z['key'] + ".json?v=" + str(rev)
+            a = urllib.urlopen(url)
+            url = "http://openlibrary.org" + z['key'] + ".json?v=" + str(rev-1)
+            b = urllib.urlopen(url)
+            c = json.JSONDecoder().decode(a.read())
+            d = json.JSONDecoder().decode(b.read())
+            csubjs = []
+            if c.has_key('subjects'):
+                for subj in c['subjects']:
+                    csubjs.append(string.lower(subj))
+            dsubjs = []
+            if d.has_key('subjects'):
+                for subj in d['subjects']:
+                    dsubjs.append(string.lower(subj))
+            if d.has_key('subjects') and ("in library" in dsubjs or "lending library" in dsubjs):
+                if c.has_key('subjects') and ("in library" in csubjs or "lending library" in csubjs):
+                    g.write("Status: fine\n")
+                else:
+                    g.write("Status: 'lending/in library' subject deleted\n")
+                    send_email("daniel.m@archive.org", ["openlibrary@archive.org"], "lending/in library subject deleted", "The 'lending library' or 'in library' subject has been deleted by %s for the work http://openlibrary.org%s with the following comment: '%s'" % (author, z['key'], y['comment']))
             else:
                 g.write("Status: fine\n")
         else:
             g.write("Status: fine\n")
+        
 
 # Update the last checked edit
 j = open("lastedit.txt", 'w')
@@ -80,7 +124,7 @@ j.write(x[0]['id'])
 j.close()
 
 # Finish writing to log
-g.write("Ended at time %r\n" % asctime())
+g.write("Ended at time %s\n" % asctime())
 g.write("Total run time = %r seconds\n" % (time() - s))
 g.close()
 os.remove("lock.txt")
