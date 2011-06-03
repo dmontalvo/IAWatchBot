54af76d7543ec2750211ba771a61d6baabc45eff
Daniel <daniel.m@archive.org>
2011-06-03 23:03:47 +0000

    added error logs

--- iawatchbot.py
+++ iawatchbot.py
@@ -9,6 +9,8 @@ import json
 import os.path
 import smtplib
 import string
+import traceback
+import sys
 
 subjlist = ['in library', 'lending library', 'accessible book', 'overdrive', 'protected daisy']
 
@@ -25,104 +27,112 @@ if os.path.exists("lock.txt"):
     exit()
 i = open("lock.txt", 'w')
 
-# Find the last checked edit
-if os.path.exists("lastedit.txt"):
-    j = open("lastedit.txt", 'r')
-    last_id = j.read()
-    j.close()
-else:
-    last_id = 0
-
 t = asctime()
 s = time()
 
-# Write to the log
-g = open("logs/%s" % t, 'w')
-g.write('Started at: %s\n' % t)
+try:
+    # Find the last checked edit
+    if os.path.exists("lastedit.txt"):
+        j = open("lastedit.txt", 'r')
+        last_id = j.read()
+        j.close()
+    else:
+        last_id = 0
 
-# Get the whitelist
-k = urllib.urlopen("http://openlibrary.org/usergroup/admin.json")
-l = json.JSONDecoder().decode(k.read())
-whitelist = []
-for member in l['members']:
-    whitelist.append(member['key'])
-m = urllib.urlopen("http://openlibrary.org/usergroup/api.json")
-n = json.JSONDecoder().decode(m.read())
-for member in n['members']:
-    whitelist.append(member['key'])
+    # Write to the log
+    g = open("logs/%s" % t, 'w')
+    g.write('Started at: %s\n' % t)
 
-# Query for recent changes
-f = urllib.urlopen("http://openlibrary.org/recentchanges.json?limit=1000")
-x = json.JSONDecoder().decode(f.read())
+    # Get the whitelist
+    k = urllib.urlopen("http://openlibrary.org/usergroup/admin.json")
+    l = json.JSONDecoder().decode(k.read())
+    whitelist = []
+    for member in l['members']:
+        whitelist.append(member['key'])
+    m = urllib.urlopen("http://openlibrary.org/usergroup/api.json")
+    n = json.JSONDecoder().decode(m.read())
+    for member in n['members']:
+        whitelist.append(member['key'])
 
-# Iterate over the recent changes
-for y in x:
-    if y['id'] <= last_id:
-        break
-    g.write("Checking id: %s\n" % y['id'])
-    author = "[None]"
-    if y['author'] is not None:
-        author = "http://openlibrary.org%s" % y['author']['key']
-    elif y['ip'] is not None:
-        author = y['ip']
-    if y['author'] is not None and y['author']['key'] in whitelist:
-        g.write("Status: fine\n")
-        continue
-    for z in y['changes']:
-        problem = False
-        rev = z['revision']
+    # Query for recent changes
+    f = urllib.urlopen("http://openlibrary.org/recentchanges.json?limit=1000")
+    x = json.JSONDecoder().decode(f.read())
 
-        # If it's an edition, check for IA ids
-        if z['key'][:9] == "/books/OL":
-            url = "http://openlibrary.org" + z['key'] + ".json?v=" + str(rev)
-            a = urllib.urlopen(url)
-            url = "http://openlibrary.org" + z['key'] + ".json?v=" + str(rev-1)
-            b = urllib.urlopen(url)
-            c = json.JSONDecoder().decode(a.read())
-            d = json.JSONDecoder().decode(b.read())
+    # Iterate over the recent changes
+    for y in x:
+        if y['id'] <= last_id:
+            break
+        g.write("Checking id: %s\n" % y['id'])
+        author = "[None]"
+        if y['author'] is not None:
+            author = "http://openlibrary.org%s" % y['author']['key']
+        elif y['ip'] is not None:
+            author = y['ip']
+        if y['author'] is not None and y['author']['key'] in whitelist:
+            g.write("Status: fine\n")
+            continue
+        for z in y['changes']:
+            problem = False
+            rev = z['revision']
+
+            # If it's an edition, check for IA ids
+            if z['key'][:9] == "/books/OL":
+                url = "http://openlibrary.org" + z['key'] + ".json?v=" + str(rev)
+                a = urllib.urlopen(url)
+                url = "http://openlibrary.org" + z['key'] + ".json?v=" + str(rev-1)
+                b = urllib.urlopen(url)
+                c = json.JSONDecoder().decode(a.read())
+                d = json.JSONDecoder().decode(b.read())
             
-            # if it had an IA id, make sure it wasn't changed/removed
-            if d.has_key("ocaid"):
-                if c.has_key("ocaid"):
-                    if c["ocaid"] != d["ocaid"]:
-                        g.write("Status: ocaid modified for %s\n" % z['key'])
+                # if it had an IA id, make sure it wasn't changed/removed
+                if d.has_key("ocaid"):
+                    if c.has_key("ocaid"):
+                        if c["ocaid"] != d["ocaid"]:
+                            g.write("Status: ocaid modified for %s\n" % z['key'])
+                            problem = True
+                            send_email("daniel.m@archive.org", ["openlibrary@archive.org"], "IA id modified", "The IA id has been modified by %s for the edition http://openlibrary.org%s with the following comment: '%s'" % (author, z['key'], y['comment']))                        
+                    else:
+                        g.write("Status: ocaid deleted from %s\n" % z['key'])
                         problem = True
-                        send_email("daniel.m@archive.org", ["openlibrary@archive.org"], "IA id modified", "The IA id has been modified by %s for the edition http://openlibrary.org%s with the following comment: '%s'" % (author, z['key'], y['comment']))                        
-                else:
-                    g.write("Status: ocaid deleted from %s\n" % z['key'])
-                    problem = True
-                    send_email("daniel.m@archive.org", ["openlibrary@archive.org"], "IA id deleted", "The IA id has been deleted by %s for the edition http://openlibrary.org%s with the following comment: '%s'" % (author, z['key'], y['comment']))
-        elif z['key'][:9] == "/works/OL":
-            url = "http://openlibrary.org" + z['key'] + ".json?v=" + str(rev)
-            a = urllib.urlopen(url)
-            url = "http://openlibrary.org" + z['key'] + ".json?v=" + str(rev-1)
-            b = urllib.urlopen(url)
-            c = json.JSONDecoder().decode(a.read())
-            d = json.JSONDecoder().decode(b.read())
-            csubjs = []
-            if c.has_key('subjects'):
-                for subj in c['subjects']:
-                    csubjs.append(string.lower(subj))
-            dsubjs = []
-            if d.has_key('subjects'):
-                for subj in d['subjects']:
-                    dsubjs.append(string.lower(subj))
-            for subj in subjlist:
-                if d.has_key('subjects') and subj in dsubjs:
-                    if not (c.has_key('subjects') and subj in csubjs):
-                        g.write("Status: %s subject deleted from %s\n" % (subj, z['key']))
-                        problem = True
-                        send_email("daniel.m@archive.org", ["openlibrary@archive.org"], "%s subject deleted" % subj, "The '%s' subject has been deleted by %s for the work http://openlibrary.org%s with the following comment: '%s'" % (subj, author, z['key'], y['comment']))
-        if not problem:
-            g.write("Status: %s fine\n" % z['key'])
+                        send_email("daniel.m@archive.org", ["openlibrary@archive.org"], "IA id deleted", "The IA id has been deleted by %s for the edition http://openlibrary.org%s with the following comment: '%s'" % (author, z['key'], y['comment']))
+            elif z['key'][:9] == "/works/OL":
+                url = "http://openlibrary.org" + z['key'] + ".json?v=" + str(rev)
+                a = urllib.urlopen(url)
+                url = "http://openlibrary.org" + z['key'] + ".json?v=" + str(rev-1)
+                b = urllib.urlopen(url)
+                c = json.JSONDecoder().decode(a.read())
+                d = json.JSONDecoder().decode(b.read())
+                csubjs = []
+                if c.has_key('subjects'):
+                    for subj in c['subjects']:
+                        csubjs.append(string.lower(subj))
+                dsubjs = []
+                if d.has_key('subjects'):
+                    for subj in d['subjects']:
+                        dsubjs.append(string.lower(subj))
+                for subj in subjlist:
+                    if d.has_key('subjects') and subj in dsubjs:
+                        if not (c.has_key('subjects') and subj in csubjs):
+                            g.write("Status: %s subject deleted from %s\n" % (subj, z['key']))
+                            problem = True
+                            send_email("daniel.m@archive.org", ["openlibrary@archive.org"], "%s subject deleted" % subj, "The '%s' subject has been deleted by %s for the work http://openlibrary.org%s with the following comment: '%s'" % (subj, author, z['key'], y['comment']))
+            if not problem:
+                g.write("Status: %s fine\n" % z['key'])
 
-# Update the last checked edit
-j = open("lastedit.txt", 'w')
-j.write(x[0]['id'])
-j.close()
+    # Update the last checked edit
+    j = open("lastedit.txt", 'w')
+    j.write(x[0]['id'])
+    j.close()
 
-# Finish writing to log
-g.write("Ended at: %s\n" % asctime())
-g.write("Total run time: %s seconds\n" % (time() - s))
-g.close()
-os.remove("lock.txt")
+    # Finish writing to log
+    g.write("Ended at: %s\n" % asctime())
+    g.write("Total run time: %s seconds\n" % (time() - s))
+    g.close()
+    os.remove("lock.txt")
+
+except:
+    error = traceback.format_exc()
+    print error
+    el = open("/var/www/errors/errorlog.txt","a")
+    el.write("%s\n%s\n\n" % (t, error))
+    os.remove("lock.txt")
